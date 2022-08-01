7071bd1f31230ffd9872d97d2135570d504ddc19
Rico <d-Rickyy-b@users.noreply.github.com>
2022-08-01 04:09:31 +0200

    feat: parse config from file

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"go-certstream-server/internal/certificatetransparency"
+	"go-certstream-server/internal/config"
 	"go-certstream-server/internal/web"
 	"log"
 )
@@ -12,9 +13,16 @@ func main() {
 	// var configFile = flag.String("config", "config.yml", "Path to config file")
 	var port = flag.Int("port", 8080, "port to listen on")
 	var networkIf = flag.String("interface", "127.0.0.1", "interface to listen on")
+	var configFile = flag.String("config", "config.yaml", "path to the config file")
 	flag.Parse()
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	conf, err := config.ReadConfig(*configFile)
+	if err != nil {
+		log.Fatalln("Error while parsing yaml file:", err)
+	}
+
 	go web.StartServer(*networkIf, *port)
 
 	watcher := certificatetransparency.Watcher{}
