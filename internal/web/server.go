6b2cc1fcfab3aa7d2fbdbe7a48fe51d95b3429d8
Rico <d-Rickyy-b@users.noreply.github.com>
2022-08-07 03:47:52 +0200

    feat: add elliptic curve cipher

--- internal/web/server.go
+++ internal/web/server.go
@@ -119,6 +119,7 @@ func (ws *WebServer) initServer() {
 		MinVersion:       tls.VersionTLS12,
 		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256, tls.X25519},
 		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
