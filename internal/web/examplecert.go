91ab6c63a7d0fd9726ade64ae1fb3953ad8b15a0
Rico <d-Rickyy-b@users.noreply.github.com>
2022-08-07 02:59:53 +0200

    refactor: move code related to example certs in own file

---
+++ internal/web/examplecert.go
@@ -0,0 +1,33 @@
+package web
+
+import (
+	"go-certstream-server/internal/certstream"
+	"net/http"
+)
+
+var exampleCert certstream.Entry
+
+// exampleFull handles requests to the /full-stream/example.json endpoint.
+// It returns a JSON representation of the full example certificate.
+func exampleFull(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(exampleCert.JSON()) //nolint:errcheck
+}
+
+// exampleLite handles requests to the /example.json endpoint.
+// It returns a JSON representation of the lite example certificate.
+func exampleLite(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(exampleCert.JSONLite()) //nolint:errcheck
+}
+
+// exampleDomains handles requests to the /domains-only/example.json endpoint.
+// It returns a JSON representation of the domain data.
+func exampleDomains(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(exampleCert.JSONDomains()) //nolint:errcheck
+}
+
+func SetExampleCert(cert certstream.Entry) {
+	exampleCert = cert
+}
--- internal/web/server.go
+++ internal/web/server.go
@@ -14,7 +14,6 @@ import (
 )
 
 var ClientHandler = BroadcastManager{}
-var exampleCert certstream.Entry
 var upgrader = websocket.Upgrader{} // use default options
 
 type WebsocketServer struct {
@@ -83,31 +82,6 @@ func setupClient(connection *websocket.Conn, subscriptionType SubscriptionType, 
 	go c.listenWebsocket()
 
 	ClientHandler.registerClient(c)
-}
-
-// exampleFull handles requests to the /full-stream/example.json endpoint.
-// It returns a JSON representation of the full example certificate.
-func exampleFull(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(exampleCert.JSON()) //nolint:errcheck
-}
-
-// exampleLite handles requests to the /example.json endpoint.
-// It returns a JSON representation of the lite example certificate.
-func exampleLite(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(exampleCert.JSONLite()) //nolint:errcheck
-}
-
-// exampleDomains handles requests to the /domains-only/example.json endpoint.
-// It returns a JSON representation of the domain data.
-func exampleDomains(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(exampleCert.JSONDomains()) //nolint:errcheck
-}
-
-func SetExampleCert(cert certstream.Entry) {
-	exampleCert = cert
 }
 
 // setupRoutes configures all the routes necessary for the webserver
